1e2477d7c937a0500eb709981adc70d3b5475cb9
mogita <me@mogita.com>
2025-08-05 22:32:30 +0800

    fix: read .env from parent path

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: '../../', // Load .env files from project root
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
